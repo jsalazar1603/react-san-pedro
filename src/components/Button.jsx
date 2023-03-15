6db6db461dd00bdb20decb8246b6f33873836952
Renzo Samuel García Cervera <renzogc745@gmail.com>
2023-03-14 21:46:09 -0500

    actualizar estilos del componente button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,9 @@
+import styles from '../styles/Button.module.css';
+
 const Button = ({content}) =>{
     return(
         <>
-            <button>{content}</button>
+            <button className={styles.content}>{content}</button>
         </>
     )
 }
